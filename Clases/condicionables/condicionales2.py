ece262a752772877e4d01f8d19568b4e9a88111f
Alejandro Pérez González <42699036+Zexceed7@users.noreply.github.com>
2020-08-02 12:29:47 -0500

    Add files via upload

---
+++ Clases/condicionables/condicionales2.py
@@ -0,0 +1,19 @@
+x=4
+y=5
+if(x==4 and y==5):
+    print("esta bien")
+
+texto= "Hola"
+texto2="Hola"
+
+#Compara si ambas condiciones se cumplen
+if(texto == "Hola" and texto2 == "Adios"):
+    print("Hasta luego")
+elif (texto=="Hola" or texto2=="Adios"):
+    print("BIen")
+
+
+
+
+
+
