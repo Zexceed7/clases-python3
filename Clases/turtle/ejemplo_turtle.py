b500970ce83b68707d011b24b01fca5cade5418f
Alejandro Pérez González <42699036+Zexceed7@users.noreply.github.com>
2020-08-02 12:30:20 -0500

    Add files via upload

--- Clases/turtle/ejemplo_turtle.py
+++ Clases/turtle/ejemplo_turtle.py
@@ -1,2 +1,33 @@
 import turtle as t
-t.forward(100)
+###########LIneas rectas
+#t.forward(100)
+#t.right(90)
+#t.forward(100)
+#t.fd(100)
+#t.setheading(90)
+#t.penup()
+#t.fd(50)
+#t.pendown()
+#t.fd(100)
+#t.mainloop()
+
+#cambiar tamaño de lienzo
+t.setup(650,500)
+#circulos
+t.pencolor('blue')
+t.circle(100)
+
+
+#puntos
+t.pensize(5)
+t.pencolor('red')
+t.dot()
+t.fd(50)
+t.dot()
+t.fd(50)
+
+
+#resetting
+t.reset()
+t.mainloop()
+
